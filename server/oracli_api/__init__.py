bf3e5e4072998f4652cc8ac5b6c0ff4f657b61b8
Ben <benlaugherty@gmail.com>
2020-02-16 22:58:34 -0800

    add routes to get all mentors and all mentees

--- server/oracli_api/__init__.py
+++ server/oracli_api/__init__.py
@@ -10,6 +10,8 @@ from oracli_api.controllers.login import Login
 from oracli_api.controllers.register import Register
 from oracli_api.controllers.test import Test
 from oracli_api.controllers.user import User
+from oracli_api.controllers.mentees import Mentees
+from oracli_api.controllers.mentors import Mentors
 
 # load environment variables
 load_dotenv()
@@ -43,3 +45,7 @@ api.add_resource(Register, '/register')
 api.add_resource(Test, '/test')
 # user route
 api.add_resource(User, '/user')
+# mentors route
+api.add_resource(Mentors, '/mentors')
+# mentees route
+api.add_resource(Mentees, '/mentees')
---
+++ server/oracli_api/controllers/mentors.py
@@ -0,0 +1,16 @@
+from flask import jsonify
+from flask_restful import Resource
+from bson import json_util
+import json
+
+from oracli_api.utils.auth import token_required
+
+import oracli_api
+
+
+class Mentors(Resource):
+    @token_required
+    def get(self, data, token):
+        # get all mentors from db and return them
+        mentors = list(oracli_api.mentors.find({}))
+        return jsonify(json.loads(json_util.dumps(mentors)))
